fb33711b7e64e39e8396710cddd958b0b3279212
a4163581 <31943609+a4163581@users.noreply.github.com>
2018-11-23 10:59:43 +0800

    Add files via upload

--- server.js
+++ server.js
@@ -3,73 +3,140 @@ const M = require('./model')
 const logger = require('koa-logger')
 const router = require('koa-router')()
 const koaBody = require('koa-body')
+const session = require('koa-session')
 
 const Koa = require('koa')
 const app = (module.exports = new Koa())
 
-const login = 'Jack'
-let index = 0    // 'Jack' 'Merry' 'Ben'
+app.keys = ['some secret hurr']
+
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  /** (number || 'session') maxAge in ms (default is 1 days) */
+  /** 'session' will result in a cookie that expires when session/browser is closed */
+  /** Warning: If a session cookie is stolen, this cookie will never expire */
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false) */
+}
 
 app.use(logger())
 app.use(koaBody())
+app.use(session(CONFIG, app))
 
 router
   .get('/', list)
-  .get('/:user/post/new', add)
   .get('/:user/posts', listpost)
+  .get('/:user/post/new', add)
   .get('/:user/post/:id', show)
-  .get('/edit', edit)
-  .get('/delete', del)
+  .get('/signup', signup)
+  .get('/login', login)
+  .get('/:user/edit/:id', edit)
+  .get('/:user/delete/:id', deletepost)
   .post('/:user/post', create)
-  .post('/editwell', editwell)
+  .post('/check', check)
+  .post('/enter', enter)
+  .post('/:user/modify/:id', modify)
 
 app.use(router.routes())
 
-async function list (ctx) {
-  const blog = M.list()
-  ctx.body = await V.list(blog, login)
-}
+// post
 
-async function add (ctx) {
-  ctx.body = await V.new(login)
+async function list (ctx) {
+  const posts = M.list()
+  const users = M.getuser()
+  ctx.body = await V.list(posts, users, ctx.session.user)
 }
 
 async function listpost (ctx) {
-  const posts = M.listpost(login)
-  ctx.body = await V.listpost(posts, login)
+  const user = ctx.params.user
+  const posts = M.listpost(user)
+  ctx.body = await V.listpost(posts, user)
+}
+
+async function add (ctx) {
+  if (ctx.session.user !== ctx.params.user) {
+    ctx.status = 401
+    ctx.body = '<p>請先<a href="/login">登入</a></p>'
+  } else {
+    const user = ctx.params.user
+    ctx.body = await V.new(user)
+  }
 }
 
 async function show (ctx) {
   const id = ctx.params.id
-  index = id
-  const post = M.get(id, login)
+  const user = ctx.params.user
+  const post = M.get(id)
   if (!post) ctx.throw(404, 'invalid post id')
-  ctx.body = await V.show(post)
+  ctx.body = await V.show(post, ctx.session.user, user)
 }
 
 async function create (ctx) {
   const post = ctx.request.body
-  M.add(post, login)
-  ctx.redirect('/')
+  const user = ctx.params.user
+  M.add(post, user)
+  ctx.redirect(`/${user}/posts`)
 }
 
+// user
+
+async function signup (ctx) {
+  ctx.body = await V.signup()
+}
+
+async function check (ctx) {
+  const user = ctx.request.body
+  if (M.check(user)) {
+    ctx.status = 401
+    ctx.body = '<p>使用者已存在，請重新<a href="/signup">註冊</a></p>'
+  } else {
+    ctx.redirect('/login')
+  }
+}
+
+async function login (ctx) {
+  ctx.body = await V.login()
+}
+
+async function enter (ctx) {
+  const user = ctx.request.body
+  if (M.login(user)) {
+    ctx.session.user = user.account
+    ctx.redirect('/')
+  } else {
+    ctx.status = 401
+    ctx.body = '<p>登入失敗，請重新<a href="/login">登入</a></p>'
+  }
+}
+
+// modify
+
 async function edit (ctx) {
-  const post = M.get(index, login)
+  const id = ctx.params.id
+  const post = M.get(id)
   ctx.body = await V.edit(post)
 }
 
-async function editwell (ctx) {
+async function modify (ctx) {
   const post = ctx.request.body
-  const targetpost = M.get(index, login)
-  targetpost.title = post.title
-  targetpost.body = post.body
-  M.editwell(targetpost, login, index)
-  ctx.redirect(`/${login}/post/${index}`)
+  const user = ctx.params.user
+  const id = ctx.params.id
+  M.modify(post, id)
+  ctx.redirect(`/${user}/post/${id}`)
 }
 
-async function del (ctx) {
-  M.del(login, index)
-  ctx.redirect(`/${login}/posts`)
+// delete
+
+async function deletepost (ctx) {
+  const id = ctx.params.id
+  const user = ctx.params.user
+  M.delete(id)
+  ctx.redirect(`/${user}/posts`)
 }
 
 if (!module.parent) {
